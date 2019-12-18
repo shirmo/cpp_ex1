70c311e7a1e08feb085ede2f89d45da36ae25100
shirmo <51527440+shirmo@users.noreply.github.com>
2019-12-18 19:33:11 +0200

    Start assignment
    import all required files

---
+++ Matrix.h
@@ -0,0 +1,17 @@
+// Matrix.h
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+/**
+ * @struct MatrixDims
+ * @brief Matrix dimensions container
+ */
+typedef struct MatrixDims
+{
+    int rows, cols;
+} MatrixDims;
+
+// Insert Matrix class here...
+
+#endif //MATRIX_H
